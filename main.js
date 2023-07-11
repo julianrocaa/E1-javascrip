ab9711883a66f277db52e21001cca2dd5a252a43
Julián <julianlepore496@gmail.com>
2023-07-10 21:56:41 -0300

    e1-js

---
+++ main.js
@@ -0,0 +1,9 @@
+let numero =5;
+function paridad(numero) {
+  if (num % 2 === 0) {
+    console.log(`El número ` + numero + ` es PAR`);
+  } else {
+    console.log(`El número ` + numero + ` es IMPAR`);
+  }
+}
+    
